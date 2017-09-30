4072c63402988e02cf513466808f924a0836c1dc
James Kingdom <james@unetworks.org>
2017-09-30 21:17:15 +0100

    Update

--- index.php
+++ index.php
@@ -1,189 +1,59 @@
 <?php session_start(); /* Starts the session */
-
-
-
 if(!isset($_SESSION['UserData']['Username'])){
-
 	header("location:login.php");
-
 	exit;
-
 }
-
 ?>
-
-
-
-
-
-
-
 <!DOCTYPE html>
+<html style="background-color:#11293a;">
 
-<html lang="en">
-
-  <head>
-
+<head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=":)">
+    <title>James's Dev Index</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.7/darkly/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic">
+    <link rel="stylesheet" href="assets/css/user.css">
+</head>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <meta name="description" content="">
-
-    <meta name="author" content="">
-
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>James's Dev</title>
-
-
-
-    <title>DevOPS</title>
-
-
-
-    <!-- Bootstrap core CSS -->
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-
-
-    <!-- Custom styles for this template -->
-
-    <link href="justified-nav.css" rel="stylesheet">
-
-  </head>
-
-
-
-  <body>
-
-
-
-    <div class="container">
-
-         <p><a class="btn btn-danger" href="logout.php">Logout</a></p>
-
-      </div>
-
-
-
-      <!-- Jumbotron -->
-
-      <div class="jumbotron">
-
-        <h1>Development Server</h1>
-
-        <p class="lead">You have reached James's dev server, view projects by clicking on the links below</p>
-
-		
-
-		
-
-      </div>
-
-
-
-      <!-- Example row of columns -->
-
-      <div class="row">
-
-      <!--  <div class="col-lg-4">
-
-          <h2>Squid Reports</h2>
-
-          <p>SARG generated reports showing squid traffic, updated via cron every 60 seconds. </p>
-
-          <p><a class="btn btn-primary" href="squid-reports" role="button">View &raquo;</a></p>
-
-        </div>-->
-
-        <div class="col-lg-4">
-
-          <h2>OpenStreetMap</h2>
-
-          <p>iD EDITOR (PORT 80)</p>
-
-          <p><a class="btn btn-primary" href="http://devops:8080" role="button">View  &raquo;</a></p>
-
-       </div>
-
-        <div class="col-lg-4">
-
-          <h2>Munin log analyse</h2>
-
-          <p>Munin</p>
-
-          <p><a class="btn btn-primary" href="http://james.glideservers.com/munin/" role="button">View  &raquo;</a></p>
-
-        </div>
-
-        <div class="col-lg-4">
-
-          <h2>SubSonic Web Server</h2>
-
-          <p>Subsonic media player, for all my music needs</p>
-
-          <p><a class="btn btn-primary" href="http://james.glideservers.com:4040" role="button">View  &raquo;</a></p>
-
-        </div>
-
-       </div>
-
+<body style="background-color:#11293a;">
+    <section class="testimonials">
+        <h1 class="text-center">James Dev</h1>
+        <p class="text-center">This is my dev server index page </p>
+        <p class="text-center text-danger"><a class="btn btn-warning" role="button" href="logout.php">Logout</a></p>
+    </section>
+    <div class="container-fluid" style="background-color:#22232a;width:93%;height:100%;">
         <div class="row">
-
-	<div class="col-lg-4">
-
-	  <h2>FileRun</h2>
-
-	  <p>Opensource File Syncing Software</p>
-
-	  <p><a class="btn btn-primary disabled" href="#" role="button">UNDER DEVELOPMENT</a></p>
-
-	  </div>       
-
-
-
-       </div>
-
-
-
-      <!-- Site footer -->
-
-      <footer class="footer">
-
-        <p>&copy; JamesKingdom 2017</p>
-
-      </footer>
-
-
-
-    </div> <!-- /container -->
-
-
-
-
-
-    <!-- Bootstrap core JavaScript
-
-    ================================================== -->
-
-    <!-- Placed at the end of the document so the pages load faster -->
-
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-
-    <script src="../../dist/js/bootstrap.min.js"></script>
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-
-  </body>
+            <div class="col-md-4 item">
+                <h2 class="text-center">Heading</h2>
+                <p class="text-center">Paragraph</p><a class="btn btn-success btn-block" role="button" href="#">Button</a></div>
+            <div class="col-md-4 item">
+                <h2 class="text-center">Heading</h2>
+                <p class="text-center">Paragraph</p><a class="btn btn-success btn-block" role="button" href="#">Button</a></div>
+            <div class="col-md-4 item">
+                <h2 class="text-center">Subsonic Music</h2>
+                <p class="text-center">Custom music libary</p><a class="btn btn-success btn-block" role="button" href="https://james.glideservers.com:4040">Subsonic </a></div>
+        </div>
+        <div class="row top-buffer">
+            <div class="col-md-4 item">
+                <h2 class="text-center">Heading</h2>
+                <p class="text-center">Paragraph</p><a class="btn btn-success btn-block" role="button" href="#">Button</a></div>
+            <div class="col-md-4 item">
+                <h2 class="text-center">Heading</h2>
+                <p class="text-center">Paragraph</p><a class="btn btn-success btn-block" role="button" href="#">Button</a></div>
+            <div class="col-md-4 item">
+                <h2 class="text-center">Heading</h2>
+                <p class="text-center">Paragraph</p><a class="btn btn-success btn-block" role="button" href="#">Button</a></div>
+        </div>
+    </div>
+    <footer class="site-footer">
+        <div class="container-fluid">
+            <p>James Kingdom © 2017</p>
+        </div>
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
 
 </html>
-
-				
