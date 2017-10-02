12b184421d2ce35a13e5fc9f2fc0874cad89b10d
JamesKingdom <james@unetworks.org>
2017-10-02 08:11:12 +0100

    Clean up code formatting for login

--- login.php
+++ login.php
@@ -1,91 +1,23 @@
 <?php session_start(); /* Starts the session */
 include ('config.php');
-
-
-	
-
-
-
 	/* Check Login form submitted */	
-
-
-
 	if(isset($_POST['Submit'])){
-
-
-
 		/* Define username and associated password array */
-
-
-
 		$logins = array('admin' => $passw);
-
-
-
-		
-
-
-
 		/* Check and assign submitted Username and Password to new variable */
-
-
-
 		$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
-
-
-
 		$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
-
-
-
-		
-
-
-
 		/* Check Username and Password existence in defined array */		
-
-
-
 		if (isset($logins[$Username]) && $logins[$Username] == $Password){
-
-
-
 			/* Success: Set session variables and redirect to Protected page  */
-
-
-
 			$_SESSION['UserData']['Username']=$logins[$Username];
-
-
-
 			header("location:index.php");
-
-
-
 			exit;
-
-
-
 		} else {
-
-
-
 			/*Unsuccessful attempt: Set error message */
-
-
-
 			$msg="<span style='color:red'>Invalid Login Details</span>";
-
-
-
 		}
-
-
-
 	}
-
-
-
 ?>
 
 
